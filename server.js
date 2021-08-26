e86c48043dff7540ef1f668dc556f889d4eda318
Leonio Santos <leoniopoint@gmail.com>
2021-08-26 16:37:27 -0300

    feitas alterações necessarias para funcionamento dos banners

--- server.js
+++ server.js
@@ -34,6 +34,29 @@ server.get('/services', (req, res) => {
 
     return res.json({services});
 })
+
+server.get('/banner', (req, res) => {
+    const banners = [
+        {
+          title: '',
+          image: 'https://i.imgur.com/UhCwg59.jpg'
+        },
+        {
+          title: '',
+          image: 'https://i.imgur.com/mdMPx5l.jpg'
+        },
+        {
+          title: '',
+          image: 'https://i.imgur.com/li9mxLH.jpg'
+        },
+        {
+          title: '',
+          image: 'https://i.imgur.com/0CsYHbm.jpg'
+        },
+      ]
+
+      return res.json({banners})
+})
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT);
