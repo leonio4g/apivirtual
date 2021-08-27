68610dc686cf9786c5c2daca8ecec5a58b80e852
Leonio Santos <leoniopoint@gmail.com>
2021-08-27 17:42:18 -0300

    implementado a rota da caterira do plano

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express')
- var bodyParser = require('body-parser')
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const swagger = require('swagger-ui-express');
+const cors = require("cors");
+const swaggerDocs = require('./swagger.json');
 const server  = express();
 
 server.use(bodyParser.urlencoded({ extended: false }))
 
 server.use(express.json());
+server.use(cors());
+
+server.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs))
 
 server.put('/changepass', (req,res) => {
     const password = "1234ab"
 
-    const comparePass = req.body;
-    if( comparePass.password === password) {
+    const comparePass = req.body.password;
+    if( comparePass === password) {
         return res.json({status: true, comparePass})
     }else {
         return res.json({status : false, comparePass})
@@ -25,11 +30,6 @@ server.get('/services', (req, res) => {
             description: "Marque consultas rapidamente",
             icon: "icomoom/iconAgendamento"
         },
-        {
-            title: "Teleconsultas",
-            description: "Faça sua consulta de casa.",
-            icon: "icomoom/iconAgendamento"
-        }
     ];
 
     return res.json({services});
@@ -56,7 +56,159 @@ server.get('/banner', (req, res) => {
       ]
 
       return res.json({banners})
-})
+});
+
+server.get('/wallet', (req, res) => {
+  const items= [
+    {
+      nom_plano_ans: "UNIV I BAS/ESP-IND/FAM- ADAPT",
+      tipo_regulamentacao: "Adaptado",
+      registro_ans: "0151",
+      abrangencia_geografica: "Nacional",
+      segmentacao_assistencial: "Ambulatorial + Hospitalar com obstetrícia",
+      tipo_de_contratacao: "Individual ou familiar",
+      nome: "MARIA SOUSA COSTA",
+      nasc: "1980-05-22T03:00:00Z",
+      cpf: "12345678978",
+      cns: 708009362338726,
+      cdcontratante: 84315,
+      cdmodalidade: 2,
+      cdplano: 1,
+      nrproposta: 12749,
+      cdtipoplano: 3,
+      nrinsccontrante: 84315,
+      nrinsccontranteorigem: 0,
+      cdcontratoorigem: 0,
+      idplanoans: 24,
+      cdformapagamento: 1,
+      nrtermoadesaoproposta: 12749,
+      cdtipocontratacao: 1,
+      mesultimoreajuste: 1,
+      cdregistroplano: 1,
+      dtalteracaoregulamentacao: "2011-08-01T03:00:00Z",
+      matricula: 201274900039,
+      nrteradesao: 12749,
+      via: 1,
+      validade: "2022-07-31T03:00:00Z",
+      inclusao: "1995-01-08T03:00:00Z",
+      possuicarencia: 0,
+      possuibonificapenaliza: 0,
+      cdusuario: 3,
+      cdunimed: 33,
+      cdunimedorigem: 0,
+      cdunimeddestino: 0,
+      cdfuncionario: " ",
+      cdgrauparentesco: 1,
+      cdsegmentoassistencial: 6,
+      usuidplanoans: 24,
+      dtexclusaoplano: null,
+      cdsexo: 0,
+      nrdias: 0,
+      cdtitular: 3,
+      nrtelefone1: "084968113-4874",
+      nrtelefone2: " ",
+      email: " ",
+      cdviatransferencia: "N",
+      dtiniciovinculo: "1995-01-08T03:00:00Z",
+      cdclassemensagem: 2,
+      dtiniciotermoadesao: "1995-01-08T03:00:00Z",
+      dsendereco: "AVENIDA EPITACIO PESSOA, 903",
+      nrendereco: 903,
+      cep: "68906812",
+      dscomplemento: " ",
+      dsbairro: "CABRALZINHO",
+      uf: "AP",
+      cidade: "MACAPA",
+      convenioanscontratada: "321044",
+      layoutcartaovirtual: "BENEFICIARIO",
+      seed: "LBKUMSDKOJKFUUSUJNIUYTSRGN4HKNSC",
+      unimedcontratada: "UNIMED JOÃO PESSOA",
+      nomecartao: "MARILES S S F DA COSTA",
+      nomesocialcartao: "MARIA SOUSA COSTA",
+      cd_unimed: 33,
+      cd_carteira_inteira: 201274900039,
+      rdatendimento: "NAO BASICA",
+      acomodacao: "INDIVIDUAL",
+      atendimento: 0033,
+      cobparctemp: "NAO HA",
+    },
+    {
+      nom_plano_ans: "UNIV I BAS/ESP-IND/FAM- ADAPT",
+      tipo_regulamentacao: "Adaptado",
+      registro_ans: "0151",
+      abrangencia_geografica: "Nacional",
+      segmentacao_assistencial: "Ambulatorial + Hospitalar com obstetrícia",
+      tipo_de_contratacao: "Individual ou familiar",
+      nome: "MARILES SOUSA SANTOS FALCONI DA COSTA",
+      nasc: "1965-06-18T03:00:00Z",
+      cpf: "51156547415",
+      cns: 708009362338726,
+      cdcontratante: 84315,
+      cdmodalidade: 2,
+      cdplano: 1,
+      nrproposta: 12749,
+      cdtipoplano: 3,
+      nrinsccontrante: 84315,
+      nrinsccontranteorigem: 0,
+      cdcontratoorigem: 0,
+      idplanoans: 24,
+      cdformapagamento: 1,
+      nrtermoadesaoproposta: 12749,
+      cdtipocontratacao: 1,
+      mesultimoreajuste: 1,
+      cdregistroplano: 1,
+      dtalteracaoregulamentacao: "2011-08-01T03:00:00Z",
+      matricula: 201274900039,
+      nrteradesao: 12749,
+      via: 1,
+      validade: "2022-07-31T03:00:00Z",
+      inclusao: "1995-01-08T03:00:00Z",
+      possuicarencia: 0,
+      possuibonificapenaliza: 0,
+      cdusuario: 3,
+      cdunimed: 33,
+      cdunimedorigem: 0,
+      cdunimeddestino: 0,
+      cdfuncionario: " ",
+      cdgrauparentesco: 1,
+      cdsegmentoassistencial: 6,
+      usuidplanoans: 24,
+      dtexclusaoplano: null,
+      cdsexo: 0,
+      nrdias: 0,
+      cdtitular: 3,
+      nrtelefone1: "084968113-4874",
+      nrtelefone2: " ",
+      email: " ",
+      cdviatransferencia: "N",
+      dtiniciovinculo: "1995-01-08T03:00:00Z",
+      cdclassemensagem: 2,
+      dtiniciotermoadesao: "1995-01-08T03:00:00Z",
+      dsendereco: "AVENIDA BEM-TE-VI, 903",
+      nrendereco: 903,
+      cep: "68906812",
+      dscomplemento: " ",
+      dsbairro: "CABRALZINHO",
+      uf: "AP",
+      cidade: "MACAPA",
+      convenioanscontratada: "321044",
+      layoutcartaovirtual: "BENEFICIARIO",
+      seed: "LBKUMSDKOJKFUUSUJNIUYTSRGN4HKNSC",
+      unimedcontratada: "UNIMED JOÃO PESSOA",
+      nomecartao: "MARILES S S F DA COSTA",
+      nomesocialcartao: "MARILES SOUSA SANTOS FALCONI DA COSTA",
+      cd_unimed: 33,
+      cd_carteira_inteira: 201274900039,
+      rdatendimento: "NAO BASICA",
+      acomodacao: "INDIVIDUAL",
+      atendimento: 0033,
+      cobparctemp: "NAO HA",
+    }
+  ];
+
+  return res.json({items})
+ 
+});
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT);
